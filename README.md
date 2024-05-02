300fdbf2f9f7a06d3c0cdd8bbfffe982c6835a2b
enigmurl <43832426+enigmurl@users.noreply.github.com>
2024-05-01 19:57:43 -0700

    Fix unit tests

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 ## Contributors and Team Members
 Manu Bhat
+
+## Expose Junit Answers
+1. I would not use a unit test to achieve this. Messaging involves a wide array of complex parts that need to work together, and hence it is more appropriate to test this in an integration test as opposed to a unit test.
+2. I would use a unit test to test this. It is a simple feature that does not need many other parts to be able to test, hence it is appropriate to use a unit test.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -8,4 +8,68 @@ import {
   isHexColor,
 } from '../code-to-unit-test/unit-test-me';
 
-// TODO - Part 2
+test('postive phone 1', () => {
+    expect(isPhoneNumber('100-200-3000')).toBe(true);
+});
+test('postive phone 2', () => {
+    expect(isPhoneNumber('(000) 200-3000')).toBe(true);
+});
+test('negative phone 1', () => {
+    expect(isPhoneNumber('dog')).toBe(false);
+});
+test('negative phone 2', () => {
+    expect(isPhoneNumber('hello')).toBe(false);
+});
+
+test('postive email 1', () => {
+    expect(isEmail('user@mail.com')).toBe(true);
+});
+test('postive email 2', () => {
+    expect(isEmail('hello@google.com')).toBe(true);
+});
+test('negative email 1', () => {
+    expect(isEmail('dog')).toBe(false);
+});
+test('negative email 2', () => {
+    expect(isEmail('hello')).toBe(false);
+});
+
+test('postive password 1', () => {
+    expect(isStrongPassword('a_A123123asdf')).toBe(true);
+});
+test('postive password 2', () => {
+    expect(isStrongPassword('b_123123_asAA')).toBe(true);
+});
+test('negative password 1', () => {
+    expect(isStrongPassword('dog')).toBe(false);
+});
+test('negative password 2', () => {
+    expect(isStrongPassword('1hello')).toBe(false);
+});
+
+
+test('postive date 1', () => {
+    expect(isDate('00/00/0000')).toBe(true);
+});
+test('postive date 2', () => {
+    expect(isDate('12/24/2000')).toBe(true);
+});
+test('negative date 1', () => {
+    expect(isDate('dog')).toBe(false);
+});
+test('negative date 2', () => {
+    expect(isDate('1hello')).toBe(false);
+});
+
+test('postive hex color 1', () => {
+    expect(isHexColor('FFFF0a')).toBe(true);
+});
+test('postive hex color 2', () => {
+    expect(isHexColor('#AAbb01')).toBe(true);
+});
+test('negative hex color 1', () => {
+    expect(isHexColor('dog')).toBe(false);
+});
+test('negative hex color 2', () => {
+    expect(isHexColor('1hello')).toBe(false);
+});
